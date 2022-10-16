e4a1edc866525603406da69223321f04dcd92399
Artem Sidorov <s.artem.ekb@gmail.com>
2022-10-16 20:48:19 +0300

    Create Shapes

---
+++ app/src/main/java/com/greencom/android/podcasts3/ui/theme/Shapes.kt
@@ -0,0 +1,5 @@
+package com.greencom.android.podcasts3.ui.theme
+
+import androidx.compose.material3.Shapes
+
+val Shapes = Shapes()
--- app/src/main/java/com/greencom/android/podcasts3/ui/theme/Theme.kt
+++ app/src/main/java/com/greencom/android/podcasts3/ui/theme/Theme.kt
@@ -35,6 +35,7 @@ fun AppTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
+        shapes = Shapes,
         typography = Typography,
         content = content,
     )
