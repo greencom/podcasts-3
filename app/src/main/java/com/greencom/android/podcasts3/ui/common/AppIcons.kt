7db3f15272867e0c1002e945f9b8e77d3d6017ff
Artem Sidorov <s.artem.ekb@gmail.com>
2022-10-27 23:09:03 +0300

    Add navigation bar icons

---
+++ app/src/main/java/com/greencom/android/podcasts3/ui/common/AppIcons.kt
@@ -0,0 +1,12 @@
+package com.greencom.android.podcasts3.ui.common
+
+import com.greencom.android.podcasts3.R
+
+object AppIcons {
+
+    const val NavigationBarPodcastsOutlined = R.drawable.ic_navigation_bar_podcasts_outlined_24
+    const val NavigationBarPodcastsFilled = R.drawable.ic_navigation_bar_podcasts_filled_24
+    const val NavigationBarDiscoverOutlined = R.drawable.ic_navigation_bar_discover_outlined_24
+    const val NavigationBarDiscoverFilled = R.drawable.ic_navigation_bar_discover_filled_24
+
+}
