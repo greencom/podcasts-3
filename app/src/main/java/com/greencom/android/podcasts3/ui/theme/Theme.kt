f72cd4b2a383a2ab79dcb9a3d7b53e2819c417af
Artem Sidorov <s.artem.ekb@gmail.com>
2022-12-03 19:35:57 +0300

    Remove default colors

--- app/src/main/java/com/greencom/android/podcasts3/ui/theme/Theme.kt
+++ app/src/main/java/com/greencom/android/podcasts3/ui/theme/Theme.kt
@@ -6,17 +6,9 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
-)
+private val DarkColorScheme = darkColorScheme()
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
-)
+private val LightColorScheme = lightColorScheme()
 
 @Composable
 fun AppTheme(
