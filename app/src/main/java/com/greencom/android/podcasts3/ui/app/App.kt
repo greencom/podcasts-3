038e32ab80f3ae4d6bb0648876af5de4795904fc
Artem Sidorov <s.artem.ekb@gmail.com>
2022-12-06 14:16:04 +0300

    Make navigation bar hideable

--- app/src/main/java/com/greencom/android/podcasts3/ui/app/App.kt
+++ app/src/main/java/com/greencom/android/podcasts3/ui/app/App.kt
@@ -4,28 +4,39 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onSizeChanged
 import androidx.navigation.compose.rememberNavController
 import com.greencom.android.podcasts3.ui.app.components.AppNavigationBar
-import com.greencom.android.podcasts3.ui.common.compositionlocalproviders.LocalNavigationBarSizeTracker
+import com.greencom.android.podcasts3.ui.common.screenbehaviors.navigationbar.LocalNavigationBarBehaviorController
+import com.greencom.android.podcasts3.ui.common.screenbehaviors.navigationbar.NavigationBarBehavior
 import com.greencom.android.podcasts3.ui.theme.AppTheme
+import com.greencom.android.podcasts3.utils.screenbehavior.DefaultScreenBehaviorController
 
 @Composable
 fun App() {
     AppTheme {
-        Box {
-            // TODO: Replace with app navigation
-            Surface(modifier = Modifier.fillMaxSize()) {}
-
-            val navigationBarSizeTracker = LocalNavigationBarSizeTracker.current
-            AppNavigationBar(
-                navController = rememberNavController(),
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .onSizeChanged { navigationBarSizeTracker.onSizeChanged(it) },
+        // TODO: Do not forget to change
+        val navigationBarBehaviorController = remember {
+            DefaultScreenBehaviorController<NavigationBarBehavior>(
+                defaultBehavior = NavigationBarBehavior.Visible(isAnimated = false)
             )
+        }
+
+        CompositionLocalProvider(
+            LocalNavigationBarBehaviorController provides navigationBarBehaviorController,
+        ) {
+            Box {
+                // TODO: Replace with app navigation
+                Surface(modifier = Modifier.fillMaxSize()) {}
+
+                AppNavigationBar(
+                    navController = rememberNavController(),
+                    modifier = Modifier.align(Alignment.BottomCenter),
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/greencom/android/podcasts3/ui/app/components/AppNavigationBar.kt
+++ app/src/main/java/com/greencom/android/podcasts3/ui/app/components/AppNavigationBar.kt
@@ -6,17 +6,57 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.IntSize
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.greencom.android.podcasts3.ui.common.compositionlocalproviders.LocalNavigationBarSizeTracker
+import com.greencom.android.podcasts3.ui.common.screenbehaviors.navigationbar.LocalNavigationBarBehaviorController
+import com.greencom.android.podcasts3.ui.common.screenbehaviors.navigationbar.NavigationBarBehavior
 import com.greencom.android.podcasts3.ui.navigation.AppNavigationItem
 import com.greencom.android.podcasts3.ui.navigation.AppNavigationItems
 import com.greencom.android.podcasts3.utils.navigation.navigateToNavigationItem
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun AppNavigationBar(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+) {
+    val navigationBarBehaviorController =
+        LocalNavigationBarBehaviorController.current
+    val navigationBarBehavior by navigationBarBehaviorController.currentBehavior
+    val isVisible = navigationBarBehavior is NavigationBarBehavior.Visible
+    val isAnimated = navigationBarBehavior.isAnimated
+
+    val navigationBarSizeTracker = LocalNavigationBarSizeTracker.current
+
+    AnimatedVisibility(
+        visible = isVisible,
+        enter = if (isAnimated) expandVertically() else EnterTransition.None,
+        exit = if (isAnimated) shrinkVertically() else ExitTransition.None,
+        modifier = modifier.onSizeChanged {
+            // TODO: Height remains 1px after the bar disappears
+            // Workaround for cases when the animation is disabled
+            val newSize = if (!isVisible && !isAnimated) IntSize.Zero else it
+            navigationBarSizeTracker.onSizeChanged(newSize)
+        },
+    ) {
+        AppNavigationBarContent(
+            navController = navController,
+            modifier = Modifier.animateEnterExit(
+                enter = if (isAnimated) slideInVertically { it } else EnterTransition.None,
+                exit = if (isAnimated) slideOutVertically { it } else ExitTransition.None,
+            ),
+        )
+    }
+}
+
+@Composable
+private fun AppNavigationBarContent(
     navController: NavHostController,
     modifier: Modifier = Modifier,
 ) {
@@ -47,6 +87,8 @@ fun AppNavigationBar(
     }
 }
 
+private val NavigationBarItemAnimationSpec = tween<Float>(durationMillis = 140)
+
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
 private fun NavigationBarItemIcon(
@@ -68,5 +110,3 @@ private fun NavigationBarItemIcon(
         )
     }
 }
-
-private val NavigationBarItemAnimationSpec = tween<Float>(durationMillis = 140)
