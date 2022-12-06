474715bccaa925331fb9970d1983ce6945775880
Artem Sidorov <s.artem.ekb@gmail.com>
2022-12-06 17:20:28 +0300

    Add QA build type

--- buildSrc/src/main/java/buildtypes/AppBuildTypes.kt
+++ buildSrc/src/main/java/buildtypes/AppBuildTypes.kt
@@ -1,6 +1,6 @@
 package buildtypes
 
-val appBuildTypes = listOf(Debug, Release)
+val appBuildTypes = listOf(Debug, Qa, Release)
 
 object Debug : BuildType("debug") {
     override val appName = "@string/app_name_debug"
@@ -8,8 +8,17 @@ object Debug : BuildType("debug") {
     override val isMinifyEnabled = false
 }
 
+object Qa : BuildType("qa") {
+    override val appName = "@string/app_name_qa"
+    override val isDebuggable = false
+    override val isMinifyEnabled = true
+    override val isLoggingEnabled = true
+}
+
 object Release : BuildType("release") {
     override val appName = "@string/app_name_release"
     override val isDebuggable = false
     override val isMinifyEnabled = true
+    override val applicationIdSuffix = ""
+    override val versionNameSuffix = ""
 }
--- buildSrc/src/main/java/buildtypes/BuildType.kt
+++ buildSrc/src/main/java/buildtypes/BuildType.kt
@@ -3,9 +3,12 @@ package buildtypes
 abstract class BuildType(val name: String) {
     abstract val appName: String
 
-    open val isDebuggable = false
-    open val isMinifyEnabled = true
+    open val isDebuggable: Boolean = false
+    open val isMinifyEnabled: Boolean = true
 
-    val applicationIdSuffix = ".$name"
-    val versionNameSuffix = "--$name"
+    open val applicationIdSuffix: String = ".$name"
+    open val versionNameSuffix: String = "--$name"
+
+    open val isLoggingEnabled: Boolean
+        get() = isDebuggable
 }
