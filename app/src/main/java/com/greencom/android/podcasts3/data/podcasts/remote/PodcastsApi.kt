9fe4272571214796da256f4e028cc7001988c581
Artem Sidorov <s.artem.ekb@gmail.com>
2022-12-07 12:12:01 +0300

    Add PodcastsApi

---
+++ app/src/main/java/com/greencom/android/podcasts3/data/podcasts/remote/PodcastsApi.kt
@@ -0,0 +1,18 @@
+package com.greencom.android.podcasts3.data.podcasts.remote
+
+import com.greencom.android.podcasts3.data.podcasts.remote.dto.GetBestPodcastsResponseDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PodcastsApi {
+
+    @GET("best_podcasts")
+    suspend fun getBestPodcasts(
+        @Query("genre_id") genreId: Int?,
+        @Query("page") page: Int,
+        @Query("language") language: String,
+        @Query("sort") sort: String,
+        @Query("safe_mode") safeMode: Int,
+    ): GetBestPodcastsResponseDto
+
+}
---
+++ app/src/main/java/com/greencom/android/podcasts3/data/podcasts/remote/dto/GetBestPodcastsResponseDto.kt
@@ -0,0 +1,28 @@
+package com.greencom.android.podcasts3.data.podcasts.remote.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GetBestPodcastsResponseDto(
+    @SerialName("id") val genreId: Int? = null,
+    @SerialName("name") val genreName: String? = null,
+    @SerialName("total") val total: Int? = null,
+    @SerialName("page_number") val pageNumber: Int? = null,
+    @SerialName("podcasts") val podcasts: List<PodcastDto>? = null,
+    @SerialName("has_next") val hasNextPage: Boolean? = null,
+    @SerialName("has_previous") val hasPreviousPage: Boolean? = null,
+    @SerialName("next_page_number") val nextPageNumber: Int? = null,
+    @SerialName("previous_page_number") val previousPageNumber: Int? = null,
+) {
+
+    @Serializable
+    data class PodcastDto(
+        @SerialName("id") val id: String? = null,
+        @SerialName("title") val title: String? = null,
+        @SerialName("description") val description: String? = null,
+        @SerialName("image") val imageUrl: String? = null,
+        @SerialName("publisher") val publisher: String? = null,
+        @SerialName("explicit_content") val explicitContent: Boolean? = null,
+    )
+}
