0ffbf6fa4ac720d5dafbf7921710020d8d01a79d
Artem Sidorov <s.artem.ekb@gmail.com>
2022-12-07 11:41:55 +0300

    Add defaultScope to BaseViewModel

--- app/src/main/java/com/greencom/android/podcasts3/ui/common/baseviewmodel/BaseViewModel.kt
+++ app/src/main/java/com/greencom/android/podcasts3/ui/common/baseviewmodel/BaseViewModel.kt
@@ -1,10 +1,15 @@
 package com.greencom.android.podcasts3.ui.common.baseviewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.plus
 
 abstract class BaseViewModel<E: Event, SE: SideEffect> : ViewModel() {
+
+    private val defaultScope = viewModelScope + Dispatchers.Default
 
     private val _sideEffects = Channel<SE>(Channel.UNLIMITED)
     val sideEffects = _sideEffects.receiveAsFlow()
