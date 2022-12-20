ae48f36da19d485a95959d4f678cd01991914c72
Artem Sidorov <s.artem.ekb@gmail.com>
2022-12-20 23:26:36 +0300

    Provide Google sign-in client with Hilt

--- app/src/main/java/com/greencom/android/podcasts3/di/FirebaseModule.kt
+++ app/src/main/java/com/greencom/android/podcasts3/di/FirebaseModule.kt
@@ -1,9 +1,13 @@
 package com.greencom.android.podcasts3.di
 
+import android.content.Context
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.firebase.auth.FirebaseAuth
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -14,5 +18,13 @@ class FirebaseModule {
     @Provides
     @Singleton
     fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
+
+    @Provides
+    @Singleton
+    fun provideGoogleSignInClient(
+        @ApplicationContext context: Context,
+    ): SignInClient {
+        return Identity.getSignInClient(context)
+    }
 
 }
