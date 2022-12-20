16d48ac7b2ea4a0bb539bf9b75b041e650f91ed1
Artem Sidorov <s.artem.ekb@gmail.com>
2022-12-21 00:53:49 +0300

    Add SignIn destination to AppNavigation

--- app/src/main/java/com/greencom/android/podcasts3/ui/app/components/AppNavigation.kt
+++ app/src/main/java/com/greencom/android/podcasts3/ui/app/components/AppNavigation.kt
@@ -4,9 +4,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import com.greencom.android.podcasts3.ui.common.tooling.Placeholder
+import com.greencom.android.podcasts3.ui.navigation.CommonDestinations
 import com.greencom.android.podcasts3.ui.navigation.graphs.PodcastsGraph
 import com.greencom.android.podcasts3.ui.navigation.graphs.discoverGraph
 import com.greencom.android.podcasts3.ui.navigation.graphs.podcastsGraph
+import com.greencom.android.podcasts3.utils.navigation.composableDestination
 
 @Composable
 fun AppNavigation(
@@ -18,6 +21,10 @@ fun AppNavigation(
         startDestination = PodcastsGraph.route,
         modifier = modifier,
     ) {
+        composableDestination(CommonDestinations.SignIn) {
+            Placeholder("SignIn")
+        }
+
         podcastsGraph(navController)
         discoverGraph(navController)
     }
