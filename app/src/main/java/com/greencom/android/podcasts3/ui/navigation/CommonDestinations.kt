60d2d85f8e557749196ff1b6f315d77d3b6b5acf
Artem Sidorov <s.artem.ekb@gmail.com>
2022-12-21 00:51:41 +0300

    Add CommonDestinations with SignIn destination

---
+++ app/src/main/java/com/greencom/android/podcasts3/ui/navigation/CommonDestinations.kt
@@ -0,0 +1,11 @@
+package com.greencom.android.podcasts3.ui.navigation
+
+import com.greencom.android.podcasts3.utils.navigation.parameterless.SimpleDestination
+
+object CommonDestinations {
+
+    object SignIn : SimpleDestination() {
+        override val routeSchema = "signIn"
+    }
+
+}
