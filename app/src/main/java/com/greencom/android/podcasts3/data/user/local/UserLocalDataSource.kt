ad7d28196f5eb4b19f25299335dde48aeb6e4ef4
Artem Sidorov <s.artem.ekb@gmail.com>
2022-12-30 21:15:53 +0300

    Add getCurrentUser() to UserDao

--- app/src/main/java/com/greencom/android/podcasts3/data/user/local/UserLocalDataSource.kt
+++ app/src/main/java/com/greencom/android/podcasts3/data/user/local/UserLocalDataSource.kt
@@ -1,9 +1,10 @@
 package com.greencom.android.podcasts3.data.user.local
 
+import com.greencom.android.podcasts3.data.user.local.db.UserDao
 import javax.inject.Inject
 
-class UserLocalDataSource @Inject constructor() {
-
-
+class UserLocalDataSource @Inject constructor(
+    private val dao: UserDao,
+) {
 
 }
--- app/src/main/java/com/greencom/android/podcasts3/data/user/local/db/UserDao.kt
+++ app/src/main/java/com/greencom/android/podcasts3/data/user/local/db/UserDao.kt
@@ -1,6 +1,17 @@
 package com.greencom.android.podcasts3.data.user.local.db
 
 import androidx.room.Dao
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
 
+// TODO: Write tests
 @Dao
-interface UserDao
+abstract class UserDao {
+
+    fun getCurrentUser(): Flow<UserEntity?> = getCurrentUserRaw().distinctUntilChanged()
+
+    @Query("SELECT * FROM ${UserEntity.TABLE_NAME}")
+    protected abstract fun getCurrentUserRaw(): Flow<UserEntity?>
+
+}
