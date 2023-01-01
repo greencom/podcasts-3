6b23be2ec3ba0a6bc7f6e87d7e6d5c3f082462bf
Artem Sidorov <s.artem.ekb@gmail.com>
2023-01-01 20:25:55 +0300

    Remove defaultDispatcherScope from BaseViewModel

--- app/src/main/java/com/greencom/android/podcasts3/utils/BaseViewModel.kt
+++ app/src/main/java/com/greencom/android/podcasts3/utils/BaseViewModel.kt
@@ -1,16 +1,10 @@
 package com.greencom.android.podcasts3.utils
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
 
 abstract class BaseViewModel<E, SE> : ViewModel() {
-
-    protected val defaultDispatcherScope =
-        CoroutineScope(viewModelScope.coroutineContext + Dispatchers.Default)
 
     private val _sideEffects = Channel<SE>(Channel.UNLIMITED)
     val sideEffects = _sideEffects.receiveAsFlow()
